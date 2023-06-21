5f68a4bc8484621f1bc2845816e458a5e67f69d6
Jitender Kumar <ohyajk@gmail.com>
2023-06-21 20:54:30 +0530

    fix state

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { PopContext } from '../context/PopContext'
 import { useContext } from 'react';
-import { useForm, ValidationError } from '@formspree/react';
 
 const Popup = () => {
 
@@ -14,23 +13,11 @@ const Popup = () => {
                     Contact Me
                     <hr className='h-2 w-full bg-secondry animate-pulse' />
                 </h2>
-                <ValidationError
-                    prefix="Email"
-                    field="email"
-                    errors={state.errors}
-                />
+                
                 <input autoComplete='none' className='px-4 py-2 outline-none w-full' type="email" name="email" id="email" placeholder='Your Email' required />
-                <ValidationError
-                    prefix="Message"
-                    field="message"
-                    errors={state.errors}
-                />
+                
                 <textarea autoComplete='none' className='px-4 py-2 outline-none w-full' placeholder='Add Message' rows="4" id="message" name="message" required></textarea>
-                {!state.succeeded && !state.submitting &&
                     <button type="submit" disabled={state.submitting} className='px-4 py-2 w-full bg-primary hover:scale-105'>Submit</button>
-                }
-                {state.succeeded && <h4>Sent Successfully...</h4>}
-                {state.submitting && <h4>Submitting</h4>}
                 <img onClick={toggleEmailPop} className='absolute right-2 top-2 h-10 cursor-pointer hover:scale-125' src="/cross.png" alt="cross" />
             </form>
         </section>
